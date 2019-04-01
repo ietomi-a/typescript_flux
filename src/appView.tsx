dd67b97ada91d5b5fef0569103d9eba7e8f93edf
aie2 <45744844+aie2@users.noreply.github.com>
2019-04-01 15:09:03 +0900

    first

---
+++ src/appView.tsx
@@ -0,0 +1,34 @@
+import * as React from "react"
+import {Actions} from "./actions"
+import {nameStore, messageStore} from "./stores"
+
+type TypeMyViewProps = {}
+type TypeMyViewState = { name: string, message: string }
+export class AppView extends React.Component<TypeMyViewProps,TypeMyViewState> {
+    constructor(props){
+	super(props)
+	this.state = {
+	    name: "",
+	    message: ""
+	}
+	nameStore.onChange = () => {
+	    this.setState({name: nameStore.name})
+	}
+	messageStore.onChange = () => {
+	    this.setState({message: messageStore.message})
+	}
+    }
+
+    render() {
+	console.log("View.render")
+	return (<div>
+		  <div>
+		    <input value={this.state.name}
+		           onChange={ (e) => Actions.changeName(e.target.value) } />
+		    <button onClick={(e) => Actions.submitName()}> submit </button>
+		  </div>
+		  <div> {this.state.message} </div>
+		</div>)
+    }
+}
+
---
+++ src/stores.tsx
@@ -0,0 +1,28 @@
+import {appDispatcher} from  "./appDispatcher"
+import {ActionType} from "./actions"
+
+type TypeNameStore = { name: string, onChange: (() => void) | null }
+export const nameStore: TypeNameStore = { name: "", onChange: null }
+
+type TypeMessageStore = { message: string, onChange: (() => void) | null }
+export const messageStore: TypeMessageStore = { message: "", onChange: null }
+
+
+// Action と Store を結びつける.
+appDispatcher.register( (payload: any) => {
+    if ( payload.actionType === ActionType.CHANGE_NAME ){
+	nameStore.name = payload.value
+	if( nameStore.onChange ){
+	    nameStore.onChange()
+	}
+    }
+})
+
+appDispatcher.register( (payload: any) => {
+    if ( payload.actionType === ActionType.SUBMIT_NAME ){
+	messageStore.message = "Mr. " + nameStore.name + ", hello."
+	if( messageStore.onChange ){
+	    messageStore.onChange()
+	}
+    }
+})
